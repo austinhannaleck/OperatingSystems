43b7a42cc469d50bff2d708c0a68d7dfed5ae7f9
Austin Hannaleck <ahannaleck1@gmail.com>
2015-04-30 10:20:56 -0400

    memmanage update

--- MemManageSim/MemoryManager.cpp
+++ MemManageSim/MemoryManager.cpp
@@ -17,7 +17,7 @@ MemoryManager::MemoryManager(const string& file)
 				int number = stoi(line.substr(0,1));
 				line = line.substr(1);
 				add(number);
-				cout << number << endl;
+				//cout << number << endl;
 			}
 		}
 	}
@@ -30,10 +30,53 @@ MemoryManager::MemoryManager(const string& file)
 
 void MemoryManager::add(int number)
 {
+	cout << "loading " << number << endl;
 
+	if(!contains(number))
+	{
+		if(size < 5)//put this only if map doesnt have number
+		{
+			m_pages.insert(pair<int, int> (number, page));
+			size++;
+			page++;
+			pageFault++;
+			cout << "page fault" << endl;
+		}
+		else
+		{
+			//eject lowest
+			cout << "fifo ejecting " << lowest << endl;
+			remove();
+			m_pages.insert(pair<int, int> (number, page));
+		}
+	}
+	else
+	{
+		cout << "page already loaded" << endl;
+	}
+	
+
+	//cout << m_pages[size] << endl;;
+
+
+	
 }
 
 void MemoryManager::remove()
 {
+	m_pages.erase(lowest);
+	lowest++;
+}
 
+bool MemoryManager::contains(int test)
+{
+
+}
+
+void MemoryManager::printQueue()
+{
+	for(auto it = m_pages.begin(); it != m_pages.end(); it++)
+	{
+		cout << (*it) << endl;
+	}
 }
--- MemManageSim/MemoryManager.h
+++ MemManageSim/MemoryManager.h
@@ -19,6 +19,12 @@ private:
 	map<int, int> m_pages;
 	void add(int number);
 	void remove();
+	bool contains(int test);
+	void printQueue();
+	int lowest = 0;
+	int size = 0;
+	int page = 0;
+	int pageFault = 0;
 
 
 };
